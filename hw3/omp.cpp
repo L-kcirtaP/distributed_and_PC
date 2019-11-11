2202c8f1a3f489930176f543ca8552741e1abffd
L-kcirtaP <116010151@link.cuhk.edu.cn>
2019-11-11 09:08:56 +0800

    gogo

--- hw3/omp.cpp
+++ hw3/omp.cpp
@@ -50,85 +50,85 @@ int main (int argc, char* argv[]){
     int X_RESN = atoi(argv[3]);             /* Argument 2: the width and height of the window */ 
     int Y_RESN = atoi(argv[3]);
 
-    Window          win;       
-    char            *window_name = "test", *display_name = NULL;                     /* initialization for a window */
-    Display         *display;
-    GC              gc;   //this is a graphic content, it could be a pixel color
-    unsigned long            valuemask = 0;
-    XGCValues       values; //value of the graphics content
-    XSizeHints      size_hints;
-    Pixmap          bitmap;
-    XSetWindowAttributes attr[1];
-    int             width, height,                  /* window size */
-                    x, y,                           /* window position */
-                    border_width,                   /*border width in pixels */
-                    display_width, display_height,  /* size of screen */
-                    screen;                         /* which screen */
+    // Window          win;       
+    // char            *window_name = "test", *display_name = NULL;                     /* initialization for a window */
+    // Display         *display;
+    // GC              gc;   //this is a graphic content, it could be a pixel color
+    // unsigned long            valuemask = 0;
+    // XGCValues       values; //value of the graphics content
+    // XSizeHints      size_hints;
+    // Pixmap          bitmap;
+    // XSetWindowAttributes attr[1];
+    // int             width, height,                  /* window size */
+    //                 x, y,                           /* window position */
+    //                 border_width,                   /*border width in pixels */
+    //                 display_width, display_height,  /* size of screen */
+    //                 screen;                         /* which screen */
 
-    if (  (display = XOpenDisplay (display_name)) == NULL ) {
-       fprintf (stderr, "drawon: cannot connect to X server %s\n",
-                            XDisplayName (display_name) );
-      exit (-1);
-      }
+    // if (  (display = XOpenDisplay (display_name)) == NULL ) {
+    //    fprintf (stderr, "drawon: cannot connect to X server %s\n",
+    //                         XDisplayName (display_name) );
+    //   exit (-1);
+    //   }
 
-    /* get screen size */
-    screen = DefaultScreen (display);
-    display_width = DisplayWidth (display, screen);
-    display_height = DisplayHeight (display, screen);
+    // /* get screen size */
+    // screen = DefaultScreen (display);
+    // display_width = DisplayWidth (display, screen);
+    // display_height = DisplayHeight (display, screen);
 
-    /* set window size */
-    width = X_RESN;
-    height = Y_RESN;
+    // /* set window size */
+    // width = X_RESN;
+    // height = Y_RESN;
 
-    /* set window position */
-    x = 0;
-    y = 0;
+    // /* set window position */
+    // x = 0;
+    // y = 0;
 
-    /* create opaque window */
+    // /* create opaque window */
 
-    border_width = 4;
-    win = XCreateSimpleWindow (display, RootWindow (display, screen),
-                          x, y, width, height, border_width, 
-                          WhitePixel (display, screen), BlackPixel (display, screen)); //Change to WhitePixel (display, screen) if you want a white background
+    // border_width = 4;
+    // win = XCreateSimpleWindow (display, RootWindow (display, screen),
+    //                       x, y, width, height, border_width, 
+    //                       WhitePixel (display, screen), BlackPixel (display, screen)); //Change to WhitePixel (display, screen) if you want a white background
 
-    size_hints.flags = USPosition|USSize;
-    size_hints.x = x;
-    size_hints.y = y;
-    size_hints.width = width;
-    size_hints.height = height;
-    size_hints.min_width = 300;
-    size_hints.min_height = 300;
+    // size_hints.flags = USPosition|USSize;
+    // size_hints.x = x;
+    // size_hints.y = y;
+    // size_hints.width = width;
+    // size_hints.height = height;
+    // size_hints.min_width = 300;
+    // size_hints.min_height = 300;
 
-    XSetNormalHints (display, win, &size_hints);
-    XStoreName(display, win, window_name);
+    // XSetNormalHints (display, win, &size_hints);
+    // XStoreName(display, win, window_name);
 
-    /* create graphics context */
-    gc = XCreateGC (display, win, valuemask, &values);
-    XSetBackground (display, gc, BlackPixel (display, screen));
-    XSetForeground (display, gc, WhitePixel (display, screen));
-    XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
+    // /* create graphics context */
+    // gc = XCreateGC (display, win, valuemask, &values);
+    // XSetBackground (display, gc, BlackPixel (display, screen));
+    // XSetForeground (display, gc, WhitePixel (display, screen));
+    // XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
 
-    attr[0].backing_store = Always;
-    attr[0].backing_planes = 1;
-    attr[0].backing_pixel = WhitePixel (display, screen);
+    // attr[0].backing_store = Always;
+    // attr[0].backing_planes = 1;
+    // attr[0].backing_pixel = WhitePixel (display, screen);
 
-    XChangeWindowAttributes(display, win, CWBackingStore | CWBackingPlanes | CWBackingPixel, attr);
+    // XChangeWindowAttributes(display, win, CWBackingStore | CWBackingPlanes | CWBackingPixel, attr);
 
-    XMapWindow (display, win);
-    XSync(display, 0);
+    // XMapWindow (display, win);
+    // XSync(display, 0);
 
-    XFlush (display);
+    // XFlush (display);
 
-    XColor color;
-    color.red=10000; //range from 0~65535
-    color.green=10000;
-    color.blue=10000;
+    // XColor color;
+    // color.red=10000; //range from 0~65535
+    // color.green=10000;
+    // color.blue=10000;
 
-    Status rc1=XAllocColor(display,DefaultColormap(display, screen),&color);
-    //set the color and attribute of the graphics content
-    XSetForeground (display, gc, color.pixel);
-    XSetBackground (display, gc, WhitePixel (display, screen));
-    XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
+    // Status rc1=XAllocColor(display,DefaultColormap(display, screen),&color);
+    // //set the color and attribute of the graphics content
+    // XSetForeground (display, gc, color.pixel);
+    // XSetBackground (display, gc, WhitePixel (display, screen));
+    // XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
 
     Body *bodies = (Body *) malloc((NUMBER_OF_BODIES+1)*sizeof(Body));
     srand(time(NULL));
