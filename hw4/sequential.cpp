64ad5c2570c62c8b4a50ccdf27ecb6f7cb97f6d7
L-kcirtaP <116010151@link.cuhk.edu.cn>
2019-11-24 10:52:49 +0800

    sequential code

---
+++ hw4/sequential.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <cstdlib>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <time.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+#define NUMBER_OF_ITERATIONS 300
+
+
+int main (int argc, char* argv[]){
+
+    int X_RESN = atoi(argv[1]);             /* Argument 2: the width and height of the window */ 
+    int Y_RESN = atoi(argv[1]);
+
+    Window          win;       
+    char            *window_name = "test", *display_name = NULL;                     /* initialization for a window */
+    Display         *display;
+    GC              gc;   //this is a graphic content, it could be a pixel color
+    unsigned long            valuemask = 0;
+    XGCValues       values; //value of the graphics content
+    XSizeHints      size_hints;
+    Pixmap          bitmap;
+    XSetWindowAttributes attr[1];
+    int             width, height,                  /* window size */
+                    x, y,                           /* window position */
+                    border_width,                   /*border width in pixels */
+                    display_width, display_height,  /* size of screen */
+                    screen;                         /* which screen */
+
+    if (  (display = XOpenDisplay (display_name)) == NULL ) {
+       fprintf (stderr, "drawon: cannot connect to X server %s\n",
+                            XDisplayName (display_name) );
+      exit (-1);
+      }
+
+    /* get screen size */
+    screen = DefaultScreen (display);
+    display_width = DisplayWidth (display, screen);
+    display_height = DisplayHeight (display, screen);
+
+    /* set window size */
+    width = X_RESN;
+    height = Y_RESN;
+
+    /* set window position */
+    x = 0;
+    y = 0;
+
+    /* create opaque window */
+
+    border_width = 4;
+    win = XCreateSimpleWindow (display, RootWindow (display, screen),
+                          x, y, width, height, border_width, 
+                          WhitePixel (display, screen), BlackPixel (display, screen)); //Change to WhitePixel (display, screen) if you want a white background
+
+    size_hints.flags = USPosition|USSize;
+    size_hints.x = x;
+    size_hints.y = y;
+    size_hints.width = width;
+    size_hints.height = height;
+    size_hints.min_width = 300;
+    size_hints.min_height = 300;
+
+    XSetNormalHints (display, win, &size_hints);
+    XStoreName(display, win, window_name);
+
+    /* create graphics context */
+    gc = XCreateGC (display, win, valuemask, &values);
+    XSetBackground (display, gc, BlackPixel (display, screen));
+    XSetForeground (display, gc, WhitePixel (display, screen));
+    XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
+
+    attr[0].backing_store = Always;
+    attr[0].backing_planes = 1;
+    attr[0].backing_pixel = WhitePixel (display, screen);
+
+    XChangeWindowAttributes(display, win, CWBackingStore | CWBackingPlanes | CWBackingPixel, attr);
+
+    XMapWindow (display, win);
+    XSync(display, 0);
+
+    XFlush (display);
+
+    XColor color;
+    color.red=10000; //range from 0~65535
+    color.green=10000;
+    color.blue=10000;
+
+    Status rc1=XAllocColor(display,DefaultColormap(display, screen),&color);
+    //set the color and attribute of the graphics content
+    XSetForeground (display, gc, color.pixel);
+    XSetBackground (display, gc, WhitePixel (display, screen));
+    XSetLineAttributes (display, gc, 1, LineSolid, CapRound, JoinRound);
+
+    double * temp = (double *) malloc ((X_RESN+1)*(Y_RESN+1)*sizeof(double));
+    double * room = (double *) malloc ((X_RESN+1)*(Y_RESN+1)*sizeof(double));
+
+    int num_points = (X_RESN+1)*(Y_RESN+1);
+
+    for (int i = 0; i < num_points; i++) {
+        room[i] = 20;
+    }
+    // for (int i = 0; i < X_RESN+1; i++) {
+    //     room[i] = 20;
+    //     room[Y_RESN*(X_RESN+1)+i] = 20;
+    // }
+
+    // for (int i = 0; i < Y_RESN+1; i++) {
+    //     room[i*(X_RESN+1)] = 20;
+    //     room[i*(X_RESN+1)+X_RESN] = 20;
+    // }
+    room[X_RESN/2] = 100;
+
+    struct timeval timeStart, timeEnd, timeSystemStart; 
+    double runTime=0, systemRunTime; 
+    gettimeofday(&timeStart, NULL );
+
+    for (int count = 0; count < NUMBER_OF_ITERATIONS; count++) {
+        int data_index = 0;
+
+        // calculate temperature for each point
+        for (int i = 1; i < X_RESN; i++) {
+            for (int j = 1; j < Y_RESN; j++) {
+                data_index = i + j*(X_RESN+1);        
+                temp[data_index] = (room[i+(j-1)*(X_RESN+1)] + room[i+(j+1)*(X_RESN+1)] + room[data_index-1] + room[data_index+1])/4;
+            }
+        }
+
+        // update temperature
+        for (int i = 1; i < X_RESN; i++) {
+            for (int j = 1; j < Y_RESN; j++) {
+                data_index = i + j*(X_RESN+1);        
+                room[data_index] = temp[data_index];
+            }
+        }
+
+        // draw the diagram
+        for (int i = 0; i < num_points; i++) {
+            if (room[i] > 20) {
+                XDrawPoint (display, win, gc, i%(X_RESN+1), floor(i/(X_RESN+1)));
+            }
+        }
+        XFlush (display);
+    }
+
+    gettimeofday( &timeEnd, NULL ); 
+    runTime = (timeEnd.tv_sec - timeStart.tv_sec ) + (double)(timeEnd.tv_usec -timeStart.tv_usec)/1000000;  
+
+    printf("Name: Liu Yang\nStudent ID: 116010151\nAssignment 3, N-Body Simulation, Sequential Implementation\n");
+    printf("Sequential %d Points RUN TIME is %lf\n", num_points, runTime); 
+
+    usleep(250000);
+    XFlush(display);
+    cout<<"finish running"<<endl;
+    sleep(10);
+    return 0;
+}
