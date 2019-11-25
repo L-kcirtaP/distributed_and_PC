6a58f313d3ead2b96e867c91c495ee1aa76e41e3
L-kcirtaP <116010151@link.cuhk.edu.cn>
2019-11-26 02:04:40 +0800

    add shanghai

---
+++ hw4/make.sh
@@ -0,0 +1,4 @@
+g++ sequential.cpp -lX11 -o sequential
+g++ omp.cpp -lX11 -fopenmp -o omp
+g++ pthread.cpp -lX11 -lpthread -o pthread
+mpic++ mpi.cpp -lX11 -o mpi
---
+++ hw4/make_server.sh
@@ -0,0 +1,4 @@
+g++ sequential.cpp -lX11 -o /code/116010151/sequential
+mpic++ mpi.cpp -lX11 -o /code/116010151/mpi
+g++ omp.cpp -lX11 -fopenmp -o /code/116010151/omp
+g++ pthread.cpp -lX11 -lpthread -o /code/116010151/pthread
